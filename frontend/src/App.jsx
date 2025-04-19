5b4204f0b1bf4b98197bd85830956956eeb53635
aakash-sachdev <aakashsachdev1992@gmail.com>
2025-04-18 19:32:03 -0500

    Frontend Structure | Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+     
+    <div className="h-screen flex justify-center items-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-indigo-400 drop-shadow-pink-400">
+        DreamMatch: Built Under Progress! ❤️‍🔥
+      </h1>
+    </div>
+  
+  );
+}
+
+export default App
