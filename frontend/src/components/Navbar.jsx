80630557e0bbf206ad80020e1ccd61f9b0e9dfa4
aakash-sachdev <aakashsachdev1992@gmail.com>
2025-05-02 13:12:22 -0500

    Implemented the useAuth in Login & Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-hot-toast'
+import { useAuth } from '../context/AuthContext'
 
 function Navbar() {
   const navigate = useNavigate()
+  const { logout } = useAuth()
 
   const handleLogout = () => {
+    logout()
+    toast.success('Logged out successfully!')
     // TODO: Proper logout logic later
     navigate('/login')
   }
