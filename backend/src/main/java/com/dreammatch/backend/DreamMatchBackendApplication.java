df74a5e365c140dfc1735134a1e5e8852583aced
aakash-sachdev <aakashsachdev1992@gmail.com>
2025-05-05 16:29:58 -0500

    Backend Created | Spring Boot | Java

---
+++ backend/src/main/java/com/dreammatch/backend/DreamMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dreammatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamMatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamMatchBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dreammatch/backend/DreamMatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreammatch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamMatchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
