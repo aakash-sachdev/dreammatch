1cf29c937b5820221291724343013a2d23338fa3
aakash-sachdev <aakashsachdev1992@gmail.com>
2025-05-22 00:45:13 -0500

    User Controller Created | HealthCheck Created | Tested with Postman

---
+++ backend/src/main/java/com/dreammatch/backend/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.dreammatch.backend.controller;
+
+import com.dreammatch.backend.model.User;
+import com.dreammatch.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Signup endpoint
+    @PostMapping("/signup")
+    public String signup(@RequestBody User user){
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+        if(existingUser.isPresent()){
+            return "Email already exists";
+        }
+        userRepository.save(user);
+        return "Signup successful";
+    }
+
+    // Login endpoint
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser.isEmpty()){
+            return "Email not found";
+        } if(!existingUser.get().getPassword().equals(user.getPassword())){
+            return "Invalid password";
+        }
+        return "Login successful";
+    }
+}
