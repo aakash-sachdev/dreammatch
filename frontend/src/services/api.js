ad20736ec89f50b96800d823e6a228a8d60ef6e2
aakash-sachdev <aakashsachdev1992@gmail.com>
2025-05-25 15:09:49 -0500

    Frontend connection with Backend || Axios

---
+++ frontend/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export const signupUser = (userData) => API.post('signup', userData)
+export const loginUser = (userData) => API.post('login', userData)
